2edc06aa1076b5605f3d9bd4ba8956e5a43541ed
Abigail Salome <salomeabigail9@gmail.com>
2024-11-09 16:59:11 +0300

    updated app.py

--- server/app.py
+++ server/app.py
@@ -1,14 +1,23 @@
 from flask import jsonify, make_response, render_template, request
-from flask_login import LoginManager, login_user, logout_user, current_user, login_required
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    current_user,
+    login_required,
+)
 from flask_restful import Resource
 from config import app, api
 from models import User
 
+
 # Home Resource
 class HomeResource(Resource):
     def get(self):
-        return make_response(jsonify({'message': 'Welcome to the API'}), 200)
-api.add_resource(HomeResource, '/api')
+        return make_response(jsonify({"message": "Welcome to the API"}), 200)
 
-if __name__ == '__main__':
+
+api.add_resource(HomeResource, "/api")
+
+if __name__ == "__main__":
     app.run(debug=True)
