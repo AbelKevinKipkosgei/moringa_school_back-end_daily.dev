f4d07a7f34a0d8c51b15ccc9dcf943e7d22dc92e
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-09 04:17:26 +0300

    Corrected the import file.

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import jsonify, make_response, render_template, request
 from flask_login import LoginManager, login_user, logout_user, current_user, login_required
 from flask_restful import Resource
 from config import app, api
-from server.user_model import User
+from user_model import User
 
 # Home Resource
 class HomeResource(Resource):
