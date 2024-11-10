27379e897912a20bedd16f981dcbe0903e6c6e42
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-10 09:08:50 +0300

    Changed table name from category to categories.

--- server/models/category_model.py
+++ server/models/category_model.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
-from subscription_model import Subscription
+from .subscription_model import Subscription
 
 class Category(db.Model, SerializerMixin):
-    __tablename__ = 'category'
+    __tablename__ = 'categories'
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False, unique=True)
@@ -14,7 +14,7 @@ class Category(db.Model, SerializerMixin):
     subscribed_at = db.Column(db.DateTime, default = db.func.now())
 
     # Relationship mapping category to content
-    contents = db.relationship('Content', back_populates='category')
+    posts = db.relationship('Content', back_populates='category')
 
     # Relationship mapping category to subscription
     subscriptions = db.relationship('Subscription', back_populates='category')
@@ -23,7 +23,7 @@ class Category(db.Model, SerializerMixin):
     subscribers = association_proxy('subscriptions', 'user', creator = lambda user_obj: Subscription(user = user_obj, subscribed_at = db.func.now()))
 
     # Serialization rules
-    serialize_rules = ('-contents.category', '-subscriptions.category')
+    serialize_rules = ('-posts.category', '-subscriptions.category')
 
     def __repr__(self):
         return f"<Category(id={self.id}, name={self.name})>"
