114cc1befe5b5681ebc8a843a0cbd94d7e3bbec2
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-11 09:32:56 +0300

    Commented out.

--- server/model_imports.py
+++ server/model_imports.py
@@ -1,10 +1,10 @@
-def get_models():
-    from user_model import User
-    from post_model import Post
-    from notification_model import Notification
-    from comment_model import Comment
-    from like_model import Like
-    from subscription_model import Subscription
-    from category_model import Category
+# def get_models():
+#     from user_model import User
+#     from post_model import Post
+#     from notification_model import Notification
+#     from comment_model import Comment
+#     from like_model import Like
+#     from subscription_model import Subscription
+#     from category_model import Category
 
-    return User, Post, Notification, Comment, Like, Subscription, Category
+#     return User, Post, Notification, Comment, Like, Subscription, Category
