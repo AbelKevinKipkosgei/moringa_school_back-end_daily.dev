4ac48926eb0b3c635b02faae08275a5a68ced28a
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-12 12:18:41 +0300

    Route files to be used.

---
+++ server/abigail_app.py
@@ -0,0 +1,15 @@
+from flask import jsonify, make_response, render_template, request
+from flask_restful import Resource
+from config import app, api, db
+from models import User, Post, Comment, Subscription, Like, Notification, Category
+
+# Home Resource
+class HomeResource(Resource):
+    def get(self):
+        return make_response(jsonify({"message": "Welcome to the API"}), 200)
+
+
+api.add_resource(HomeResource, "/api")
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
