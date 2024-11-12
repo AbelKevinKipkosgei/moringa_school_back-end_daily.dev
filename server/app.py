ea4641fcd4c3915717acbe82b8d8fdc213b7a2e7
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-12 12:01:49 +0300

    Removed unnecessary imports.

--- server/app.py
+++ server/app.py
@@ -1,11 +1,4 @@
 from flask import jsonify, make_response, render_template, request
-from flask_login import (
-    LoginManager,
-    login_user,
-    logout_user,
-    current_user,
-    login_required,
-)
 from flask_restful import Resource
 from config import app, api, db
 from models import User, Post, Comment, Subscription, Like, Notification, Category
@@ -14,7 +7,6 @@ from models import User, Post, Comment, Subscription, Like, Notification, Catego
 class HomeResource(Resource):
     def get(self):
         return make_response(jsonify({"message": "Welcome to the API"}), 200)
-
 
 api.add_resource(HomeResource, "/api")
 
