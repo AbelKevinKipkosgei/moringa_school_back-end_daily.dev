e0f4794cc1a01545951291a5fb0457617974dfb7
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-11-16 10:10:48 +0300

    Added seeding logic to handle media_url.

--- server/seed_post.py
+++ server/seed_post.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import cloudinary
 import cloudinary.uploader
 from sqlalchemy import text
-from models import Post, User, Category
+from models import Post, User, Category, Wishlist
 from config import app, db
 
 
@@ -26,20 +26,15 @@ def seed_posts_table():
             user1 = User.query.filter_by(username="abel_soi").first()
             user2 = User.query.filter_by(username="john_doe").first()
             user3 = User.query.filter_by(username="jane_smith").first()
+            user5 = User.query.filter_by(username="sandra_bullock").first()
+            user6 = User.query.filter_by(username="felicity_jones").first()
+            user7 = User.query.filter_by(username="audrey_hepburn").first()
 
             # Fetch categories
             machine_learning = Category.query.filter_by(name="Machine Learning").first()
             data_science = Category.query.filter_by(name="Data Science").first()
             frontend = Category.query.filter_by(name="Frontend").first()
             backend = Category.query.filter_by(name="Backend").first()
-
-            # Configuration
-            cloudinary.config( 
-            cloud_name = "dgfolnzcl", 
-            api_key = "325447839224753", 
-            api_secret = "U64bWoq9hlMWmKhJSkHOx98OAVk",
-            secure=True
-            )
 
             # Create new posts
             posts_data = [
@@ -51,7 +46,8 @@ def seed_posts_table():
                     "approved": True,
                     "user": user1,
                     "category": machine_learning,
-                    "likes_count": 20
+                    "likes_count": 20,
+                    "media_url": "https://woolyss.com/f/avc-aac-nerdist-friends.mp4"
                 },
                 {
                     "title": "Data Science with Python",
@@ -59,9 +55,10 @@ def seed_posts_table():
                     "thumbnail_url": "https://i.pinimg.com/1200x/14/cb/c1/14cbc10e848a3e5e794c11b57bf1ba3c.jpg",
                     "body": "Learn how to use Python for data science.",
                     "approved": True,
-                    "user": user1,
+                    "user": user5,
                     "category": data_science,
-                    "likes_count": 30
+                    "likes_count": 30,
+                    "media_url": None
                 },
                 {
                     "title": "Frontend Development with React",
@@ -71,7 +68,8 @@ def seed_posts_table():
                     "approved": True,
                     "user": user2,
                     "category": frontend,
-                    "likes_count": 75
+                    "likes_count": 75,
+                    "media_url": "https://woolyss.com/f/avc-aac-big-buck-bunny.m4v"
                 },
                 {
                     "title": "Backend Development with NodeJS",
@@ -81,7 +79,8 @@ def seed_posts_table():
                     "approved": True,
                     "user": user3,
                     "category": backend,
-                    "likes_count": 50
+                    "likes_count": 50,
+                    "media_url": None
                 },
                 {
                     "title": "JavaScript for Frontend Development",
@@ -89,9 +88,10 @@ def seed_posts_table():
                     "thumbnail_url": "https://i.pinimg.com/564x/0e/4f/dc/0e4fdce8ac22e09688c580e5bc4dcd7d.jpg",
                     "body": "Learn the basics of JavaScript and frontend development.",
                     "approved": True,
-                    "user": user2,
+                    "user": user7,
                     "category": frontend,
-                    "likes_count": 100
+                    "likes_count": 100,
+                    "media_url": "https://woolyss.com/f/av1-nosound-chimera.mp4"
                 },
                 {
                     "title": "The AI Ted Talk",
@@ -99,10 +99,22 @@ def seed_posts_table():
                     "thumbnail_url": "https://i.pinimg.com/1200x/e8/b5/13/e8b513bb64949c29fe3f34316e2572de.jpg",
                     "body": "Learn about AI and its applications.",
                     "approved": True,
-                    "user": user3,
+                    "user": user6,
                     "category": machine_learning,
-                    "likes_count": 25
-                }      
+                    "likes_count": 25,
+                    "media_url": "https://woolyss.com/f/audio-sample.mp3"
+                },
+                {
+                    "title": "The Blockchain",
+                    "post_type": "video",
+                    "thumbnail_url": "https://i.pinimg.com/736x/97/81/55/97815516ed640908a393f7d0c6c4544e.jpg",
+                    "body": "Blockchain with Solidity.",
+                    "approved": True,
+                    "user": user1,
+                    "category": machine_learning,
+                    "likes_count": 25,
+                    "media_url": "https://woolyss.com/f/av1-opus-sita.webm"
+                } 
             ]
 
             # Add each post to the session
@@ -117,26 +129,43 @@ def seed_posts_table():
                     )
 
                     # Print URL of the uploaded image
-                    image_url = upload_result['secure_url']
-                    print(f" Thumbnail uploaded: {image_url}")
-
-                    # Create post with uploaded thumbnail
-                    new_post = Post(
-                        title=post_data['title'],
-                        post_type=post_data['post_type'],
-                        thumbnail_url=image_url,
-                        body=post_data['body'],
-                        approved=post_data['approved'],
-                        user_id=post_data['user'].id,
-                        category_id=post_data['category'].id,
-                        likes_count=post_data['likes_count']
-                    )
-
-                    # Add new post to the session
-                    db.session.add(new_post)
+                    thumbnail_url = upload_result['secure_url']
+                    print(f" Thumbnail uploaded: {thumbnail_url}")
                 except Exception as e:
                     print(f"Error uploading thumbnail for {post_data['title']}: {e}")
-                    db.session.rollback()
+                    thumbnail_url = post_data['thumbnail_url']
+
+                # Determine media upload settings based on post_type
+                media_url = post_data['media_url']
+                if post_data['post_type'] in ['video', 'audio'] and media_url:
+                    try:
+                        upload_result = cloudinary.uploader.upload(
+                            media_url,
+                            resource_type='video' if post_data["post_type"] == 'video' else 'raw',
+                            folder=f"media/{post_data['post_type']}",
+                            public_id=f"{post_data['title']}_media"
+                        )
+                        media_url =  upload_result['secure_url']
+                        print(f"Media uploaded: {media_url}")
+                    except Exception as e:
+                        print(f"Error uploading media for {post_data['title']}: {e}")
+                        media_url = post_data['media_url']
+
+                # Create post with uploaded thumbnail
+                new_post = Post(
+                    title=post_data['title'],
+                    post_type=post_data['post_type'],
+                    thumbnail_url=thumbnail_url,
+                    media_url=media_url,
+                    body=post_data['body'],
+                    approved=post_data['approved'],
+                    user_id=post_data['user'].id,
+                    category_id=post_data['category'].id,
+                    likes_count=post_data['likes_count']
+                )
+
+                # Add new post to the session
+                db.session.add(new_post)
 
             # Commit all changes
             db.session.commit()
