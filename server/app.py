85b07a3a1ea6ba592a140a355ddc537db20326f2
Wangui <elswangui@gmail.com>
2024-11-17 18:07:54 +0300

    added user role as an identity when creating the token in the login resource

--- server/app.py
+++ server/app.py
@@ -55,8 +55,8 @@ class LoginResource(Resource):
             return {"message": "Invalid credentials"}, 401
         
         # Create JWT token
-        access_token = create_access_token(identity=user.id)
-        refresh_token = create_refresh_token(identity=user.id)
+        access_token = create_access_token(identity={"id": user.id, "role": user.role})
+        refresh_token = create_refresh_token(identity={"id": user.id, "role": user.role})
         return {
             'access_token': access_token,
             'refresh_token': refresh_token
